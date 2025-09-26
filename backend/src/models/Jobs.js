7b2b40d4c2d61e6fa17862dfd829936ae5af78b6
vanshika raina <vanshika8727@gmail.com>
2025-09-26 10:36:46 +0530

    timestamp

--- backend/src/models/Jobs.js
+++ backend/src/models/Jobs.js
@@ -10,6 +10,8 @@ const jobSchema = new mongoose.Schema({
   deadline: Date,
   postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   acceptedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null } // ✅ new field
-});
+},
+  { timestamps: true }
+);
 
 export default mongoose.model("Job", jobSchema);
