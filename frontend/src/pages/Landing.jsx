01321bf5447d386be1211e2f91df55068b3a20de
Vanshika Raina <vanshika8727@gmail.com>
2025-11-12 10:17:00 +0530

    added our location in landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -40,6 +40,20 @@ export default function Landing() {
     </div>
     </section>
 
+    <section className="location-map">
+        <h2>Find Us Here</h2>
+        <iframe
+          title="CampusGig Location"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3437.1749717999796!2d76.65720287482759!3d30.51609109607186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fc32344a6e2d7%3A0x81b346dee91799ca!2sChitkara%20University!5e0!3m2!1sen!2sin!4v1762922379648!5m2!1sen!2sin"
+          width="100%"
+          height="400"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </section>
+
     <section className="testimonials">
     <h2>What Students Say</h2>
     <div className="testimonial-cards">
